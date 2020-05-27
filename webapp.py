21f4d9934d0d6f712b76ba09019836be288a035b
Camille <cfmarcojos@gmail.com>
2020-05-26 23:19:57 -0700

    update all html files to implement in flask

--- webapp.py
+++ webapp.py
@@ -1,3 +1,116 @@
+from flask import Flask, render_template
+from flask import request, redirect
+from db_connector.db_connector import connect_to_database, execute_query
+#create the web application
+webapp = Flask(__name__)
+
+@webapp.route('/')
+def index():
+    return render_template('index.html')
+
+@webapp.route('/counselors', methods=['POST','GET'])
+#the name of this function is just a cosmetic thing
+def browse_counselors():
+    print("Fetching and rendering counselors web page")
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        query = "SELECT * from Counselors;"
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+        return render_template('counselors.html', rows=result)
+    elif request.method == 'POST':
+        print("Adding an counselor")
+        fname = request.form['firstName'] 
+        lname = request.form['lastName'] 
+        shelterID = request.form['shelterID'] 
+        
+        query = 'INSERT INTO Counselors (first_name, last_name, shelterID) VALUES (%s,%s,%s)'    
+        data = (fname, lname, shelterID)
+
+        db_connection = connect_to_database()
+        execute_query(db_connection, query, data)
+        return redirect('/counselors')
+    
+@webapp.route('/applications')
+#the name of this function is just a cosmetic thing
+def browse_applications():
+    print("Fetching and rendering applications web page")
+    db_connection = connect_to_database()
+    query = "SELECT app_num, app_date, Adopters.first_name, Adopters.last_name, petID1, petID2, petID3 FROM Applications INNER JOIN Adopters ON Applications.adopterID = Adopters.adopterID;"
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template('applications.html', rows=result)
+
+@webapp.route('/adoption_details', methods=['POST','GET'])
+#the name of this function is just a cosmetic thing
+def browse_adoption_details():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        print("Fetching and rendering adoption details web page")
+        query = "SELECT AdoptionDetails.app_num, Adopters.first_name, Adopters.last_name, Applications.counselorID, Dogs.name AS Dog_Name, app_status from AdoptionDetails INNER JOIN Applications ON AdoptionDetails.app_num = Applications.app_num INNER JOIN Adopters ON Applications.adopterID = Adopters.adopterID INNER JOIN Dogs ON AdoptionDetails.petID = Dogs.petID;"
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+        return render_template('adoption_details.html', rows=result)
+    elif request.method == 'POST':
+        print("Adding an adoption detail")
+        appID = request.form['appID'] 
+        petID = request.form['petID'] 
+        appstatus = request.form['appStatus'] 
+        
+        query = 'INSERT INTO AdoptionDetails (app_num, petID, app_status) VALUES (%s,%s,%s)'    
+        data = (appID, petID, appstatus)
+
+        db_connection = connect_to_database()
+        execute_query(db_connection, query, data)
+        return redirect('/adoption_details')
+
+
+@webapp.route('/add_application', methods=['POST','GET'])
+def add_application():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        query = 'SELECT shelterID, name from Shelters'
+        shelters = execute_query(db_connection,query).fetchall()
+        print(shelters)
+
+        query = 'SELECT adopterID, adopterID from Adopters'
+        adopters = execute_query(db_connection,query).fetchall()
+        print(adopters)
+
+        query = 'SELECT counselorID, counselorID from Counselors'
+        counselors = execute_query(db_connection,query).fetchall()
+        print(counselors)
+
+        query = 'SELECT petID, petID from Dogs'
+        pets = execute_query(db_connection,query).fetchall()
+        print(counselors)
+        
+        return render_template('add_application.html', shelters = shelters, adopters = adopters, counselors = counselors, pets = pets)
+    elif request.method == 'POST':
+        print("Adding an application")
+        date = request.form['appDate'] 
+        adopterID = request.form['adopterID'] 
+        shelterID = request.form['shelterID'] 
+        counselorID = request.form['counselorID'] 
+        petID1 = request.form['petID1'] 
+        petID2 = request.form['petID2'] 
+        petID3 = request.form['petID3'] 
+        meet_greet = request.form['meetGreet'] 
+        num_adults = request.form['numAdults'] 
+        num_kids = request.form['numChildren'] 
+        num_pets = request.form['numAnimals'] 
+        home_status = request.form['homeStatus'] 
+        home_type = request.form['homeType'] 
+
+        query = 'INSERT INTO Applications (app_date, shelterID, counselorID, adopterID, petID1, petID2, petID3, meet_greet, num_adults, num_children, num_pets, home_type, home_status) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'    
+        data = (date, shelterID, counselorID, adopterID, petID1, petID2, petID3, meet_greet, num_adults, num_kids, num_pets, home_type, home_status)
+
+        db_connection = connect_to_database()
+        execute_query(db_connection, query, data)
+  
+        return redirect('/applications')
+
+
 @webapp.route('/dogs')
 #the name of this function is just a cosmetic thing
 def browse_dogs():
@@ -61,8 +174,3 @@ def add_new_dog():
         data = (shelterID, name, birthday, gender, breed, size, status, energy, coat, color, dogsOK, catsOK, kidsOK)
         execute_query(db_connection, query, data)
         return redirect('/dogs')
-
-
-
-
-
